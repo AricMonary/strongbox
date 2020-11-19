f128280b2bb06578658d0dd6aa0bde4629ca33ab
AricMonary <aric.monary@gmail.com>
2020-11-18 16:40:34 -0800

    AbstractLayoutProvider CC Improvement

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/layout/AbstractLayoutProvider.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/layout/AbstractLayoutProvider.java
@@ -109,78 +109,7 @@ public abstract class AbstractLayoutProvider<T extends ArtifactCoordinates>
         Map<RepositoryFileAttributeType, Object> result = new HashMap<>();
         for (RepositoryFileAttributeType repositoryFileAttributeType : attributeTypes)
         {
-            Object value;
-            switch (repositoryFileAttributeType)
-            {
-            default:
-                Map<RepositoryFileAttributeType, Object> attributesLocal;
-                value = null;
-
-                break;
-            case CHECKSUM:
-                value = isChecksum(repositoryPath);
-
-                break;
-            case TEMP:
-                value = repositoryPath.isAbsolute()
-                        && repositoryPath.startsWith(repositoryPath.getFileSystem()
-                                                                   .getRootDirectory()
-                                                                   .resolve(LayoutFileSystem.TEMP));
-
-                break;
-            case TRASH:
-                value = repositoryPath.isAbsolute()
-                        && repositoryPath.startsWith(repositoryPath.getFileSystem()
-                                                                   .getRootDirectory()
-                                                                   .resolve(LayoutFileSystem.TRASH));
-
-                break;
-            case METADATA:
-                value = isArtifactMetadata(repositoryPath);
-
-                break;
-            case ARTIFACT:
-                attributesLocal = getRepositoryFileAttributes(repositoryPath,
-                                                              RepositoryFileAttributeType.CHECKSUM);
-
-                boolean isChecksum = Boolean.TRUE.equals(attributesLocal.get(RepositoryFileAttributeType.CHECKSUM));
-                boolean isDirectory = Files.isDirectory(repositoryPath);
-
-                value = !isChecksum && !isDirectory;
-
-                break;
-            case COORDINATES:
-                attributesLocal = getRepositoryFileAttributes(repositoryPath,
-                                                              RepositoryFileAttributeType.ARTIFACT);
-
-                boolean isArtifact = Boolean.TRUE.equals(attributesLocal.get(RepositoryFileAttributeType.ARTIFACT));
-
-                value = isArtifact ? getArtifactCoordinates(repositoryPath) : null;
-                break;
-            case RESOURCE_URL:
-                value = resolveResource(repositoryPath);
-
-                break;
-            case ARTIFACT_PATH:
-                value = RepositoryFiles.relativizePath(repositoryPath);
-
-                break;
-
-            case STORAGE_ID:
-                value = repositoryPath.getRepository().getStorage().getId();
-
-                break;
-
-            case REPOSITORY_ID:
-                value = repositoryPath.getRepository().getId();
-
-                break;
-            case EXPIRED:
-                value = false;
-
-                break;
-
-            }
+            Object value = getObjectFromAttributeType(repositoryPath, repositoryFileAttributeType);
             if (value != null)
             {
                 result.put(repositoryFileAttributeType, value);
@@ -188,6 +117,83 @@ public abstract class AbstractLayoutProvider<T extends ArtifactCoordinates>
         }
 
         return result;
+    }
+
+    @org.jetbrains.annotations.Nullable
+    private Object getObjectFromAttributeType(RepositoryPath repositoryPath, RepositoryFileAttributeType repositoryFileAttributeType) throws IOException {
+        Object value;
+        switch (repositoryFileAttributeType)
+        {
+        default:
+            Map<RepositoryFileAttributeType, Object> attributesLocal;
+            value = null;
+
+            break;
+        case CHECKSUM:
+            value = isChecksum(repositoryPath);
+
+            break;
+        case TEMP:
+            value = repositoryPath.isAbsolute()
+                    && repositoryPath.startsWith(repositoryPath.getFileSystem()
+                                                               .getRootDirectory()
+                                                               .resolve(LayoutFileSystem.TEMP));
+
+            break;
+        case TRASH:
+            value = repositoryPath.isAbsolute()
+                    && repositoryPath.startsWith(repositoryPath.getFileSystem()
+                                                               .getRootDirectory()
+                                                               .resolve(LayoutFileSystem.TRASH));
+
+            break;
+        case METADATA:
+            value = isArtifactMetadata(repositoryPath);
+
+            break;
+        case ARTIFACT:
+            attributesLocal = getRepositoryFileAttributes(repositoryPath,
+                                                          RepositoryFileAttributeType.CHECKSUM);
+
+            boolean isChecksum = Boolean.TRUE.equals(attributesLocal.get(RepositoryFileAttributeType.CHECKSUM));
+            boolean isDirectory = Files.isDirectory(repositoryPath);
+
+            value = !isChecksum && !isDirectory;
+
+            break;
+        case COORDINATES:
+            attributesLocal = getRepositoryFileAttributes(repositoryPath,
+                                                          RepositoryFileAttributeType.ARTIFACT);
+
+            boolean isArtifact = Boolean.TRUE.equals(attributesLocal.get(RepositoryFileAttributeType.ARTIFACT));
+
+            value = isArtifact ? getArtifactCoordinates(repositoryPath) : null;
+            break;
+        case RESOURCE_URL:
+            value = resolveResource(repositoryPath);
+
+            break;
+        case ARTIFACT_PATH:
+            value = RepositoryFiles.relativizePath(repositoryPath);
+
+            break;
+
+        case STORAGE_ID:
+            value = repositoryPath.getRepository().getStorage().getId();
+
+            break;
+
+        case REPOSITORY_ID:
+            value = repositoryPath.getRepository().getId();
+
+            break;
+        case EXPIRED:
+            value = false;
+
+            break;
+
+        }
+        return value;
     }
 
     public URL resolveResource(RepositoryPath repositoryPath)
